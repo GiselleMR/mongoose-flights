cd0ddef243ab517eeede3c482649bf56a0f8e684
GiselleMR <giselle.rubin1@gmail.com>
2023-02-22 13:57:33 -0500

    big push

---
+++ routes/flights.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+// All these routes start with "/flights"
+
+//GET /flights (render index of flights route)
+router.get('/', flightsCtrl.index);
+// GET /flights/new
+router.get('/new', flightsCtrl.new);
+// GET /flights/:id (display details page for flight
+router.get('/:id', flightsCtrl.show);
+
+// POST /flights (handle new flight posted)
+router.post('/', flightsCtrl.create); 
+
+// DELETE
+//router.delete('/:id', flightsCtrl.delete);
+
+module.exports = router;
